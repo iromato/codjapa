80320890a9e70eeeb20bad8733b9e35b8e8dc67b
iromato <iromato@hotmail.com>
2018-09-21 13:15:47 +0000

    app

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+
+get '/' do
+	  "codeops v2 NG fucker+ "
+end
+
+get '/customer' do
+	  "customer - codeops"
+end
+
+get '/api/v1/auth' do
+	  "api auth"
+end
+
+get '/api/doc' do
+	  "api doc"
+end
+
+get '/users' do
+	  "show users"
+end
+
+get '/api/version' do
+	  "1.1.8"
+end
+
+get '/api/hostname' do
+	  Socket.gethostname
+end
